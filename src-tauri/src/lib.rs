0cc67c29f983aa2d3d600ef7a5d3c8f4abe35589
Carlos Galarza <carloslfu@gmail.com>
2024-11-05 15:34:12 -0500

    http and store plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -59,6 +59,8 @@ fn sqlite_execute(query: &str, params: Vec<libsql::Value>) -> Result<(), String>
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             run_code,
