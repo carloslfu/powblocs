6a775d1745d6d4358621a9695f322cdb1a3f349e
Carlos Galarza <carloslfu@gmail.com>
2024-11-05 16:20:17 -0500

    include serialization of values on the bootstrap file

--- src-tauri/src/deno/bootstrap.js
+++ src-tauri/src/deno/bootstrap.js
@@ -1,7 +1,7 @@
 import { return_value } from "ext:core/ops";
 
 function returnValue(value) {
-  return_value(value);
+  return_value(JSON.stringify(value));
 }
 
 globalThis.RuntimeExtension = { returnValue };
