b0960949fbffd048897991aab8e95477c528d806
Carlos Galarza <carloslfu@gmail.com>
2024-12-15 12:03:49 -0500

    WIP fix code gen

--- src/engine/prompts.ts
+++ src/engine/prompts.ts
@@ -2,24 +2,6 @@ export function generateFunctionBlockPrompt(description: string) {
   return `You create blocks of well defined self-contained code in TypeScript that runs on Deno that fulfills the user's description:
 <description>${description}</description>
 
-Put all of the code in a single function and run it.
-
-At the end use \`Pow.returnValue(result)\`, where result is the value you want to return. He is an example:
-
-<code>
-import * as cowsay from "https://esm.sh/cowsay@1.6.0";
-
-let result = cowsay.say({ text: "🤠 🚀" });
-
-// count to 100 and wait 1 second between each number
-for (let i = 0; i < 100; i++) {
-  Pow.send("progress", { progress: i });
-  await Pow.sleep(1000);
-}
-
-Pow.returnValue(result);
-</code>
-
 Pow is a global object that contains the PowBlocs runtime functionality, here are the methods:
 - Pow.send(eventName: string, data: Record<string, any>): sends an event to the PowBlocs runtime. Use for any output that needs streaming.
 - Pow.returnValue(result: Record<string, any>): returns the result to the PowBlocs runtime. Use it for any output that doesn't need streaming, the final result.
@@ -30,7 +12,38 @@ Pow is a global object that contains the PowBlocs runtime functionality, here ar
 - Pow.actionName: the name of the action that is currently being executed. This is set and internally used by the PowBlocs runtime.
 - Pow.actionData: the data of the action that is currently being executed. This is set and internally used by the PowBlocs runtime.
 
-This script is re-executed every time the user runs an action.
+He is an example:
+
+<code>
+import * as cowsay from "https://esm.sh/cowsay@1.6.0";
+
+Pow.registerAction("main", () => {
+  let result = cowsay.say({ text: "🤠 🚀" });
+
+  // count to 100 and wait 1 second between each number
+  for (let i = 0; i < 100; i++) {
+    Pow.send("progress", { progress: i });
+    await Pow.sleep(1000);
+  }
+
+  Pow.returnValue(result);
+});
+
+Pow.registerAction("cowsay", (text: string) => {
+  let result = cowsay.say({ text });
+
+  Pow.returnValue(result);
+});
+
+Pow.registerAction("count", (to: number) => {
+  for (let i = 0; i < to; i++) {
+    Pow.send("progress", { progress: i });
+    await Pow.sleep(1000);
+  }
+});
+</code>
+
+The generated code is re-executed every time the user runs an action.
 
 Generate the code only, no intro, no explanation, no markdown, just the code inside <code></code> tags.`;
 }
